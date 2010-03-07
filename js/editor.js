2df3cec15e4a8c687d1893e28b2b3ed1c1ad2e3c
John Du Hart <john@compwhizii.net>
2010-03-06 23:51:27 -0500

    New editor JS for the bottom panels Added History layout

--- js/editor.js
+++ js/editor.js
@@ -1,10 +1,39 @@
-jQuery.fn.fadeToggle = function(speed, easing, callback) {
-   return this.animate({opacity: 'toggle'}, speed, easing, callback);
-};
-
-function showProtection()
+function reportSubmit()
 {
-	$('#protect_options').fadeToggle();
+	var radios = $('#report_form #reason_radio');
+	var canSubmit = false;
+	
+	for(i=0;i<radios.length;i++)
+	{
+		if(radios[i].checked == true)
+		{
+			if(radios[i].value == '9999')
+			{
+				canSubmit = ($('#report_form #other_box').val() != '');
+			}
+			else
+			{
+				canSubmit = true;
+			}
+			break;
+		}
+	}
+	
+	if(canSubmit)
+	{
+		$('#report').html('<p>Thank you, your report has been submitted.</p>');
+		var height = $('#report').height() + 34;
+        
+        $('.panels').animate({
+          height: height
+        }, 500);
+	}
 	
 	return false;
 }
+
+$(document).ready(function (){
+	$("#report_form").submit(function() {
+		return reportSubmit();
+	});
+});
